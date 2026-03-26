33f10e094b64d8c51e733ea9b562cf7e9b151a6b
Dave Gallant <davegallant@proton.me>
2026-03-25 20:12:07 -0400

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "site development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            hugo
+            just
+          ];
+
+        };
+      }
+    );
+}
