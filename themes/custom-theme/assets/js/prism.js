5f2731df46633dec748d11a4f760f8996e595c49
Dave Gallant <davegallant@proton.me>
2026-03-29 12:09:16 -0400

    Fix copy button on code snippets

--- themes/custom-theme/assets/js/prism.js
+++ themes/custom-theme/assets/js/prism.js
@@ -1,11 +1,24 @@
 import Prism from "prismjs";
 
-{{ range $.Site.Params.prism.languages }}
-  import "prismjs/components/prism-{{ . }}";
-{{ end }}
+// Languages
+import "prismjs/components/prism-markup";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-csharp";
+import "prismjs/components/prism-hcl";
+import "prismjs/components/prism-ignore";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-markdown";
+import "prismjs/components/prism-powershell";
+import "prismjs/components/prism-toml";
+import "prismjs/components/prism-yaml";
 
-{{ range $.Site.Params.prism.plugins }}
-  import "prismjs/plugins/{{ . }}/prism-{{ . }}";
-{{ end }}
+// Plugins (order matters: toolbar must load before copy-to-clipboard)
+import "prismjs/plugins/normalize-whitespace/prism-normalize-whitespace";
+import "prismjs/plugins/toolbar/prism-toolbar";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
+import "prismjs/plugins/command-line/prism-command-line";
 
 Prism.highlightAll();
